0e02a73fe2c624215c00c8c6d89c4b192ec2010e
Joshua Urrea <jurrea619@gmail.com>
2026-05-25 18:00:44 -0700

    Finish 08 Remove From Array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let newArray = arr.filter(num => !args.includes(num));
+    return newArray;
 };
 
 // Do not edit below this line

146c4abf79a7b043bd8233eb7b75ec22415b57c2
Joshua Urrea <jurrea619@gmail.com>
2026-05-25 20:58:13 -0700

    Finish 09 Sum All

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let min = num1;
+    let max = num2;
+    let sum = 0;
 
+    if(!Number.isInteger(min) || !Number.isInteger(max)){
+        return "ERROR"
+    }
+
+    if(min < 0 || max < 0){
+        return "ERROR";
+    }
+
+    if(min > max){
+        temp = max;
+        max = min;
+        min = temp;
+    }
+    
+    while(min <= max){
+        sum += min;
+        min++;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line

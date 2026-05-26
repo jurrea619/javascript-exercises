034d5b56460fa273ced9098a0e85aa67bd599a2f
Joshua Urrea <jurrea619@gmail.com>
2026-05-25 21:10:55 -0700

    Finish 10 Leap Years

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    switch (year % 4){
+        case 0:
+            if(year % 100 == 0){
+                if (year % 400 == 0) return true;
+                return false;
+            }
+            return true;
+            break;
 
+        default:
+            return false;
+            break;
+    }
 };
 
 // Do not edit below this line

63e9517825729245f4b8a6b0ad8004bcf3cf0cc2
Joshua Urrea <jurrea619@gmail.com>
2026-06-08 21:31:08 -0700

    Finish 16 Find The Oldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    //return oldest person object
+    let oldestAge = 0;
+    let oldestPerson = {};
+    for (let currentPerson of arr){
+        // debugger;
+        let currentAge = 0;
+        // if no death given, calculate current year instead
+        if(!Object.hasOwn(currentPerson, "yearOfDeath")){
+            currentPerson.yearOfDeath = new Date().getFullYear();
+        }
+        // calculate age
+        currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+        if (currentAge > oldestAge){
+            oldestAge = currentAge;
+            oldestPerson = currentPerson;
+        }
+    };
+    return oldestPerson;
 };
 
 // Do not edit below this line
